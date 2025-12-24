dcb2c0ec13541c28b42243faa6d1b3cf92f44be3
Ektasinha25 <154995448+Ektasinha25@users.noreply.github.com>
2025-12-24 21:42:22 +0530

    Add files via upload

---
+++ assignment 2/Person.cs
@@ -0,0 +1,229 @@
+﻿namespace SalaryProcessingSystem
+{
+    class Person
+    {
+        private string Name { get; set; } 
+        private int Age { get; set; }
+
+        public Person() :this("",0)
+        {
+            
+        }
+        public Person(String name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void Accept()
+        {
+            Console.WriteLine("Enter Name :");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter Age :");
+            Age = int.Parse(Console.ReadLine());
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine($"Name : {this.Name}, Age : {this.Age}");
+        }
+
+    }
+
+    class Emp : Person
+    {
+        public int Id { get; set; }
+        public double Salary { get; set; }
+
+        public Emp() :this("",0,0,0.0)
+        {
+            
+        }
+
+        public Emp(string name, int age, int id, double salary)
+            : base(name, age)
+        {
+
+            Id = id;
+            Salary = salary;
+        }
+
+        public void Accept()
+        {
+            base.Accept();
+            Console.WriteLine("Enter Id :");
+            this.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Salary :");
+            this.Salary = double.Parse(Console.ReadLine());
+        }
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Emp: Id : {this.Id}, Salary : {this.Salary} ");
+        }
+
+        public virtual double CalcSal()
+        {
+            return Salary;
+        }
+
+        public static double CalcTotalSal(Emp[] emps) { 
+        double total = 0;
+            foreach (Emp emp in emps)
+            {
+                total += emp.Salary;
+            }
+            return total;
+        }
+
+    }
+  
+    class Labor : Emp
+    {
+        public int Hours { get; set; }
+        public int Rate { get; set; }
+        
+        public Labor() : this("", 0,0, 0.0, 0 ,0)
+        {
+            this.Hours = 0;
+            this.Rate = 0;
+        }
+        public Labor(string name, int age, int id, double salary,int hour, int rate ) : base(name, age , id, salary)
+        {
+            this.Hours = hour;
+            this.Rate = rate;
+        }
+
+        public void Accept()
+        {
+            base.Accept();
+            Console.WriteLine("Enter Hours :");
+            this.Hours = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Rate :");
+            this.Rate = int.Parse(Console.ReadLine());
+        }
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Labor: Hours : {this.Hours}, Rate : {this.Rate} ");
+        }
+        public override double CalcSal()
+        {
+            return this.Hours * this.Rate;
+        }
+
+
+    }
+
+    class Manager : Emp
+    {
+        public int Bonus{ get; set; }
+
+
+        public Manager() : this("", 0, 0, 0.0, 0) 
+        {
+            this.Bonus = 0;
+        }
+        public Manager(string name, int age, int id, double salary, int bonus) : base(name, age, id, salary)
+        {
+          this.Bonus = bonus;
+        }
+
+        public void Accept()
+        {
+            base.Accept();
+            Console.WriteLine("Enter Bonus :");
+            this.Bonus = int.Parse(Console.ReadLine());
+            
+        }
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Manager: Bonus : {this.Bonus}");
+        }
+
+        public override double CalcSal()
+        {
+            return this.Salary + this.Bonus;
+        }
+
+
+    }
+
+    class Salesman : Emp
+    {
+        public int Comm { get; set; }
+
+
+        public Salesman() : this("", 0, 0, 0.0,0)
+        {
+            this.Comm = 0;
+        }
+        public Salesman(string name, int age, int id, double salary, int comm) : base(name, age, id, salary)
+        {
+            this.Comm= comm;
+        }
+
+        public void Accept()
+        {
+            base.Accept();
+            Console.WriteLine("Enter Comm :");
+            this.Comm = int.Parse(Console.ReadLine());
+
+        }
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Salesman: Comm : {this.Comm}");
+        }
+
+        public override double CalcSal()
+        {
+            return this.Salary + this.Comm;
+        }
+
+
+    }
+
+
+    class Clerk : Emp
+    {
+        public Clerk() : base()
+        {
+            
+        }
+
+        public void Accept()
+        {
+            base.Accept();
+        }
+        public void Display()
+        {
+            base.Display();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Person person = new Person("abc", 50);
+
+            Emp emp = new Emp("bac",50,1,5000);    
+
+            Labor labor = new Labor("cdb", 50, 2, 200,10,500);  
+
+            Manager manager = new Manager("xyz", 50, 3, 20000, 10000);
+
+            Salesman salesman = new Salesman("cvb",45,4,4000,4000);
+
+            Clerk clerk = new Clerk();
+
+            Emp[] emps = new Emp[5] { emp, labor,manager,salesman, clerk };
+
+            Console.WriteLine(Emp.CalcTotalSal(emps));
+             
+
+        }
+    }
+}
---
+++ assignment 2/student.cs
@@ -0,0 +1,138 @@
+﻿namespace Assignment2Q2
+{
+
+    struct Student
+    {
+        //public string Name {  get; set; }
+        //public bool Gender { get; set; }
+        //public int Age { get; set; }
+        //public int Std { get; set; }
+        //public char Div { get; set; }
+
+        //public double Marks { get; set; }
+
+        string _name;
+        bool _gender;
+        int _age;
+        int _std;
+        char _div;
+        double _marks;
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+        public bool Gender
+        {
+            get
+            {
+                return this._gender;
+            }
+            set
+            {
+                this._gender = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+            set { this._age = value; }
+        }
+        public int Std
+        {
+            get
+            {
+                return this._std;
+            }
+            set
+            {
+                this._std = value;
+            }
+        }
+        public char Div
+        {
+            get
+            {
+                return this._div;
+            }
+            set
+            {
+                this._div = value;
+
+            }
+        }
+        public double Marks
+        {
+            get
+            {
+                return this._marks;
+            }
+            set
+            {
+                this._marks = value;
+            }
+        }
+        
+
+
+        public Student()
+        {
+
+        }
+
+        public Student(string name, bool gender, int age, int std, char div, double Marks)
+        {
+            this._name = name;
+            this._gender = gender;
+            this._age = age;
+            this._std = std;
+            this._div = div;
+            this._marks = Marks;
+
+        }
+
+       
+
+
+        public void AcceptDetails()
+        {
+            Console.WriteLine("Enter Name :");
+            this._name = Console.ReadLine();
+            Console.WriteLine("Enter Gender :");
+            this._gender = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Age :");
+            this._age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Std :");
+            this._std = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Div :");
+            this._div = char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Marks :");
+            this._marks = double.Parse(Console.ReadLine());
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name : {this._name} Gender : {this._gender} Age : {this._age} Std : {this._std} Div : {this._div} Marks : {this._marks} ");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            //by default access specifier of structure members are private
+            
+        }
+    }
+}
