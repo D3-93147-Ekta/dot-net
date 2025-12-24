94ce9757d71838a9c5e66f13c6fdd55db5a1e0ac
Ektasinha25 <154995448+Ektasinha25@users.noreply.github.com>
2025-12-24 14:49:16 +0530

    Add files via upload

---
+++ assignment 1/Student.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class Student
+    {
+        private string name;
+        private bool gender;
+        private int age;
+        private int std;
+        private char div;
+        private double marks;
+
+        public Student() { }
+
+        public Student(string name, bool gender, int age, int std, char div, double marks)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+            this.std = std;
+            this.div = div;
+            this.marks = marks;
+        }
+
+        public string getName() { return name; }
+        public int getAge() { return age; } 
+        public int getStd() { return std; } 
+        public char getDiv() { return div; }
+        public bool getGender() { return gender; }
+        public double getMarks() { return marks;}
+        public void setAge(int age) {  this.age = age; }
+        public void setStd(int std) {  this.std = std; }
+        public void setDiv(char div) {  this.div = div; }
+        public void setMarks(double marks) {  this.marks = marks; }
+        public void setGender(bool gender) {  this.gender = gender; }
+
+        public void AcceptRecord()
+        {
+            Console.WriteLine("enter name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("enter age");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter gender");
+            gender = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter std");
+            std = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter div");
+            div = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter marks");
+            marks = double.Parse(Console.ReadLine());
+        }
+
+        public void DisplayRecord()
+        {
+            Console.WriteLine("name :" + this.name);
+            Console.WriteLine("age :" + this.age);
+            Console.WriteLine("gender :" + this.gender);
+            Console.WriteLine("std: "+ this.std);
+            Console.WriteLine("div: " + this.div);
+            Console.WriteLine("marks: "+ this.marks);
+        }
+
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.AcceptRecord();
+            student.DisplayRecord();    
+        }
+    }
+}
---
+++ assignment 1/calculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Program
+    {   
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter 1st number");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter 2nd number");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter operator numbers");
+            char op = char.Parse(Console.ReadLine());
+
+            double result;
+
+            switch (op)
+            {
+                case '+':
+                    result = x + y;
+                    Console.WriteLine(result);
+                    break;
+
+                case '-':
+                    result = x - y;
+                    Console.WriteLine(result);
+                    break;
+
+                case '*':
+                    result = x * y;
+                    Console.WriteLine(result);
+                    break;
+
+                case '/':
+                    result = x / y;
+                    Console.WriteLine(result);
+                    break;
+
+                default:
+                    Console.WriteLine("invalid input");
+                    break;
+            }
+
+        }
+    }
+}
